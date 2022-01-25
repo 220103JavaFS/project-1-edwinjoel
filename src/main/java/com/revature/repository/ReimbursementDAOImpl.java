a2ed01e31cbc3b836b625b1bb8a2cfd61576f213
Edwin Feliz <edwin.feliz@revature.net>
2022-01-25 11:50:12 -0500

    modified:   src/main/java/com/revature/repository/ReimbursementDAOImpl.java

--- src/main/java/com/revature/repository/ReimbursementDAOImpl.java
+++ src/main/java/com/revature/repository/ReimbursementDAOImpl.java
@@ -1,4 +1,49 @@
 package com.revature.repository;
 
-public class ReimbursementDAOImpl {
+import com.revature.model.Reimbursement;
+import com.revature.model.Status;
+
+import java.util.ArrayList;
+
+public class ReimbursementDAOImpl implements ReimbursementDAO{
+
+    @Override
+    public ArrayList<Reimbursement> getAllReimbursements() {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Reimbursement> getAllReimbursementsByAuthor(int authorUserId) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Reimbursement> getAllReimbursementsByResolver(int resolverUserId) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Reimbursement> getAllReimbursementsByStatus(Status.State status) {
+        return null;
+    }
+
+    @Override
+    public Reimbursement getReimbursementById(int reimbId) {
+        return null;
+    }
+
+    @Override
+    public boolean updateReimbursement(Reimbursement reimbursement) {
+        return false;
+    }
+
+    @Override
+    public boolean addReimbursement(Reimbursement reimbursement) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteReimbursement(int reimbId) {
+        return false;
+    }
 }
--- src/main/java/com/revature/service/ReimbursementService.java
+++ src/main/java/com/revature/service/ReimbursementService.java
@@ -1,4 +1,52 @@
 package com.revature.service;
 
+import com.revature.model.Reimbursement;
+import com.revature.model.Status;
+import com.revature.repository.ReimbursementDAO;
+import com.revature.repository.ReimbursementDAOImpl;
+
+import java.util.ArrayList;
+
 public class ReimbursementService {
+    private ReimbursementDAO reimbursementDAO = new ReimbursementDAOImpl();
+
+    public ReimbursementService() {
+    }
+
+    public ReimbursementService(ReimbursementDAO reimbursementDAO) {
+        this.reimbursementDAO = reimbursementDAO;
+    }
+
+    public ArrayList<Reimbursement> getAllReimbursements(){
+        return reimbursementDAO.getAllReimbursements();
+    }
+
+    public ArrayList<Reimbursement> getAllReimbursementsByAuthor(int authorUserId){
+        return reimbursementDAO.getAllReimbursementsByAuthor(authorUserId);
+    }
+
+    public ArrayList<Reimbursement> getAllReimbursementsByResolver(int resolverUserId){
+        return reimbursementDAO.getAllReimbursementsByResolver(resolverUserId);
+    }
+
+    public ArrayList<Reimbursement> getAllReimbursementsByStatus(Status.State status){
+        return reimbursementDAO.getAllReimbursementsByStatus(status);
+    }
+
+    public Reimbursement getReimbursementById(int reimbId){
+        return reimbursementDAO.getReimbursementById(reimbId);
+    }
+
+    public boolean updateReimbursement(Reimbursement reimbursement){
+        return true;
+    }
+
+    public boolean addReimbursement(Reimbursement reimbursement){
+        return true;
+    }
+
+    public boolean deleteReimbursement(int reimbId){
+        return true;
+    }
+
 }
