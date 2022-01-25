05cb79ad54ed23b96fb6a036a10ea9d5e19223dc
JBOhge <ohgejb@gmail.com>
2022-01-25 11:45:09 -0600

    login service

--- src/main/java/com/revature/repository/UserDAOImpl.java
+++ src/main/java/com/revature/repository/UserDAOImpl.java
@@ -1,4 +1,38 @@
 package com.revature.repository;
 
-public class UserDAOImpl {
+import com.revature.model.User;
+
+import java.util.ArrayList;
+
+public class UserDAOImpl implements UserDAO{
+
+    @Override
+    public ArrayList<User> getAllUsers() {
+        return null;
+    }
+
+    @Override
+    public User getUser(String username) {
+        return null;
+    }
+
+    @Override
+    public User getUserById(int userId) {
+        return null;
+    }
+
+    @Override
+    public boolean updateUser(User user) {
+        return false;
+    }
+
+    @Override
+    public boolean addUser(User user) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteUser(String username) {
+        return false;
+    }
 }
--- src/main/java/com/revature/service/LoginService.java
+++ src/main/java/com/revature/service/LoginService.java
@@ -1,4 +1,23 @@
 package com.revature.service;
 
+import com.revature.model.User;
+import com.revature.repository.UserDAO;
+import com.revature.repository.UserDAOImpl;
+
 public class LoginService {
+
+    private UserDAO userDAO = new UserDAOImpl();
+
+    public LoginService() {
+    }
+
+    public LoginService(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
+
+    public User login(String username, String password){
+        return null;
+    }
+
+
 }
