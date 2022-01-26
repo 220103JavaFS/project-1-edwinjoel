dd01f1c8816bb37f2a9d1cb608622d95665b9abb
JBOhge <ohgejb@gmail.com>
2022-01-26 15:16:37 -0600

    JDBC class

---
+++ src/main/java/com/revature/JDBCPostgreSQLConnection.java
@@ -0,0 +1,28 @@
+package com.revature;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JDBCPostgreSQLConnection {
+
+    private static String url2 = "jdbc:postgresql://javafs220103ohgejb.ccidemni0rly.us-east-2.rds.amazonaws.com:5432/postgres";
+
+    private static String userName = System.getenv("SQLUsername");
+    private static String password = System.getenv("SQLPassword");
+
+    public static Connection getConnection() throws SQLException{
+
+            try {
+                Class.forName(("org.postgresql.Driver"));
+
+            }catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+
+
+           return DriverManager.getConnection(url2, userName, password);
+    }
+
+}
