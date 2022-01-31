b922af64187af0eae089faf6d3d57b8d2efd08aa
JBOhge <ohgejb@gmail.com>
2022-01-31 15:36:33 -0600

    Reimbursement.js replace table every button press

--- src/main/resources/html/Reimbursements.js
+++ src/main/resources/html/Reimbursements.js
@@ -1,66 +1,68 @@
-let table = document.getElementById("table");
-let btn = document.getElementById("btn");
-btn.addEventListener("click", fetchFunc);
+let table = document.getElementById('table');
+let oldTableBody = document.getElementById('tbody');
+let newTableBody = document.createElement('tbody');
+newTableBody.id = 'tbody';
+let btn = document.getElementById('btn');
+btn.addEventListener('click', fetchFunc);
 
-async function fetchFunc(){
-    console.log("entered fetchFunc")
-    let url = 'http://localhost:7000/reimbursements'
-    
-    let response = await fetch(url, {method:'GET'})
+async function fetchFunc() {
+  //get the new references everybutton press
+  oldTableBody = document.getElementById('tbody');
 
-    if(response.status === 200){
-        let data = await response.json()
-        console.log(data)
-        renderData(data)
-    }
-    else{
-        console.log("Something went wrong")
-    }
+  //create a new <tbody> element and set id="tbody"
+  newTableBody = document.createElement('tbody');
+  newTableBody.id = 'tbody';
+
+  console.log('entered fetchFunc');
+  let url = 'http://localhost:7000/reimbursements';
+
+  let response = await fetch(url, { method: 'GET' });
+
+  if (response.status === 200) {
+    let data = await response.json();
+    console.log(data);
+    renderData(data);
+  } else {
+    console.log('Something went wrong');
+  }
 }
 
-function renderData(data){
+function renderData(data) {
+  for (let reimb of data) {
+    let newRow = document.createElement('tr');
 
-    for(let reimb of data){
-        let newRow = document.createElement("tr")
-        let id = document.createElement("td")
-        let amount = document.createElement("td")
-        let submitted = document.createElement("td")
-        let resolved = document.createElement("td")
-        let description = document.createElement("td")
-        let authorId = document.createElement("td")
-        let resolverId = document.createElement("td")
-        let type = document.createElement("td")
-        let status = document.createElement("td")
-    
-        newRow.appendChild(id)
-        newRow.appendChild(amount)
-        newRow.appendChild(submitted)
-        newRow.appendChild(resolved)
-        newRow.appendChild(description)
-        newRow.appendChild(authorId)
-        newRow.appendChild(resolverId)
-        newRow.appendChild(type)
-        newRow.appendChild(status)
+    let id = document.createElement('td');
+    let amount = document.createElement('td');
+    let submitted = document.createElement('td');
+    let resolved = document.createElement('td');
+    let description = document.createElement('td');
+    let authorId = document.createElement('td');
+    let resolverId = document.createElement('td');
+    let type = document.createElement('td');
+    let status = document.createElement('td');
 
-        id.innerText = reimb.id
-        amount.innerText = reimb.amount
-        submitted.innerText = reimb.timeSubmitted
-        resolved.innerText = reimb.timeResolved
-        description.innerText = reimb.description
-        authorId.innerText = reimb.authorUserId
-        resolverId.innerText = reimb.resolverUserId
-        type.innerText = reimb.type
-        status.innerText = reimb.status
+    newRow.appendChild(id);
+    newRow.appendChild(amount);
+    newRow.appendChild(submitted);
+    newRow.appendChild(resolved);
+    newRow.appendChild(description);
+    newRow.appendChild(authorId);
+    newRow.appendChild(resolverId);
+    newRow.appendChild(type);
+    newRow.appendChild(status);
 
-        table.appendChild(newRow)
+    id.innerText = reimb.id;
+    amount.innerText = reimb.amount;
+    submitted.innerText = reimb.timeSubmitted;
+    resolved.innerText = reimb.timeResolved;
+    description.innerText = reimb.description;
+    authorId.innerText = reimb.authorUserId;
+    resolverId.innerText = reimb.resolverUserId;
+    type.innerText = reimb.type;
+    status.innerText = reimb.status;
 
-    }
-    
+    newTableBody.appendChild(newRow);
+  }
 
-
-
-
+  oldTableBody.parentNode.replaceChild(newTableBody, oldTableBody);
 }
-
-
-
